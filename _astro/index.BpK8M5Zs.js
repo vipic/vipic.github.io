9a172ada72627b5aa0114ac19a8d1000084582c6
vipic <read_xms@hotmail.com>
2024-03-14 06:26:27 +0000

    Deploying to main from  @ 83a7ad660cb72e261b18eed4aace712cce05f22e 🚀

---
+++ _astro/index.BpK8M5Zs.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["_astro/index.Dgj1-jPt.js","_astro/_astro_assets.ClAUbKgO.js","_astro/astro/assets-service.DcMRmNXG.js","_astro/hoisted.BLL4Jfoh.js","_astro/render-template.3xIYiyox.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{_ as t}from"./astro/assets-service.DcMRmNXG.js";async function o(){return t(()=>import("./index.Dgj1-jPt.js"),__vite__mapDeps([0,1,2,3,4]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
